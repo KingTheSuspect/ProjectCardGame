91ef94b1a1f6559a376a4a7b927c5ff2043e8d4b
KingTheSuspect <demironurcem@hotmail.com>
2024-02-27 01:04:14 +0300

    StartScrollTextUpdate
    Başlangıçta text gözükmeme fixlendi

--- Assets/Scripts/ScrollCount.cs
+++ Assets/Scripts/ScrollCount.cs
@@ -5,15 +5,18 @@ using TMPro;
 public class ScrollCount : MonoBehaviour
 {
     public TextMeshProUGUI slideCountText;
+    int savedScrollCount;
+    int incrreasedSlideCount;
     void Start()
     {
-        
+        savedScrollCount = PlayerPrefs.GetInt("SavedScrollCount", 0);
+        slideCountText.text = savedScrollCount.ToString();
     }
     
     public void IncreaseSlideCount()
     {
-      int savedScrollCount = PlayerPrefs.GetInt("SavedScrollCount",0);
-      int incrreasedSlideCount = savedScrollCount + 1;
+      savedScrollCount = PlayerPrefs.GetInt("SavedScrollCount",0);
+      incrreasedSlideCount = savedScrollCount + 1;
       PlayerPrefs.SetInt("SavedScrollCount",incrreasedSlideCount);
       slideCountText.text = incrreasedSlideCount.ToString();
     }
